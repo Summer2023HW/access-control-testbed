cb8e57b7ac7e9fce62804e6840fa14703bf5a3cb
Mac <39278228+Reithger@users.noreply.github.com>
2019-05-10 15:21:22 -0300

    Minor change to receive universally

--- receive.py
+++ receive.py
@@ -1,6 +1,6 @@
 import socket
 
-UDP_IP = "192.168.2.69" #ip of the receiving machine
+UDP_IP = "0.0.0.0" #ip of the receiving machine
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,4 +8,4 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:             #listens continuously
   data, addr = sock.recvfrom(1024)  #specify 1024 bytes received
-  print "received message:", data   #do something with result in data
+  print ("received message:", data)   #do something with result in data
