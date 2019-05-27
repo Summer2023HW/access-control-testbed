bfa67203388b685d5d246e767145fa6344c545b9
Mac <39278228+Reithger@users.noreply.github.com>
2019-05-27 10:16:09 -0300

    tentative

--- Device/home_device.py
+++ Device/home_device.py
@@ -1,4 +1,7 @@
 import socket
+import sys
+sys.path.insert(0, "../Library")
+from access import *
 
 smartmeters = []
 TCP_IP = input("Arbiter IP: ")
@@ -57,88 +60,6 @@ def listen(conn):
       connect_socket(new_sock, info[2])
       new_sock.connect((info[2], TCP_PORT))
       smartmeters.append((info[1], new_sock,))
-
-#-------  Generic Below  ----------------------------------------------------------------------
-
-'''
-Manage the creation of a socket; setting initial values
-Returns Socket
-'''
-
-def make_socket():
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  return sock
-
-'''
-Manage the closing of a socket; calling shutdown and then closing
-'''
-
-def close_socket(sock):
-  try:
-    sock.shutdown(socket.SHUT_RDWR)
-    sock.close()
-  except:
-    print("Error in Socket Closing - Potentially non-fatal")
-
-'''
-Manage the binding of a socket to listen to a defined ip address ('' for universal) at a default port with
-a specified number of live connections on that socket permitted.
-Returns Boolean
-'''
-
-def bind_socket(sock, ip, num_connections):
-  try:
-    sock.bind((ip, TCP_PORT))
-    sock.listen(num_connections)
-    print("Successful binding of socket to: " + ip)
-    return True
-  except:
-    print("Failure to bind local socket to: " + ip)
-    return False
-
-'''
-Manage the connecting of a socket to a defined ip address and default port
-Returns Boolean
-'''
-
-def connect_socket(sock, ip):
-  try:
-    sock.connect((ip, TCP_PORT))
-    print("Successful connection to ip: " + ip)
-    return True
-  except:
-    print("Failure to connect to ip: " + ip)
-    return False
-
-'''
-Manage the sending of a message by a defined socket
-Returns Boolean
-'''
-
-def send(sock, message):
-  try:
-    sock.send(message.encode())
-    return True
-  except:
-    print("Failure to send message via socket at ip: " + str(re.findall("\d+\.\d+\.\d+\.\d+", str(sock))))
-    return False
-
-'''
-Authorize authentification key received from a message
-Returns Boolean
-'''
-
-def authorize(info):
-  return info == "auth"
-
-'''
-Generate authentification key to send with messages
-Returns String
-'''
-
-def authenticate():
-  return "auth"
 
 #----------------------------------------------------------------------------------------------
 
--- Smartmeter/smartmeter_water.py
+++ Smartmeter/smartmeter_water.py
@@ -1,6 +1,9 @@
 import socket
 import _thread
 import re
+import sys
+sys.path.insert(0, "../Library")
+from access import *
 
 ''' Value representing total consumed water as recorded by this SmartMeter '''
 water = 0
@@ -65,88 +68,6 @@ Upon request, inform the caller of the status of this Smart Meter
 def respond_status (sock):
   send(sock, "Water Usage: " + str(water))
   close_socket(sock)
-
-#-------  Generic Below  ----------------------------------------------------------------------
-
-'''
-Manage the creation of a socket; setting initial values
-Returns Socket
-'''
-
-def make_socket():
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-  return sock
-
-'''
-Manage the closing of a socket; calling shutdown and then closing
-'''
-
-def close_socket(sock):
-  try:
-    sock.shutdown(socket.SHUT_RDWR)
-    sock.close()
-  except:
-    print("Error in Socket Closing - Potentially non-fatal")
-
-'''
-Manage the binding of a socket to listen to a defined ip address ('' for universal) at a default port with
-a specified number of live connections on that socket permitted.
-Returns Boolean
-'''
-
-def bind_socket(sock, ip, num_connections):
-  try:
-    sock.bind((ip, TCP_PORT))
-    sock.listen(num_connections)
-    print("Successful binding of socket to: " + ip)
-    return True
-  except:
-    print("Failure to bind local socket to: " + ip)
-    return False
-
-'''
-Manage the connecting of a socket to a defined ip address and default port
-Returns Boolean
-'''
-
-def connect_socket(sock, ip):
-  try:
-    sock.connect((ip, TCP_PORT))
-    print("Successful connection to ip: " + ip)
-    return True
-  except:
-    print("Failure to connect to ip: " + ip)
-    return False
-
-'''
-Manage the sending of a message by a defined socket
-Returns Boolean
-'''
-
-def send(sock, message):
-  try:
-    sock.send(message.encode())
-    return True
-  except:
-    print("Failure to send message via socket at ip: " + str(re.findall("\d+\.\d+\.\d+\.\d+", str(sock))))
-    return False
-
-'''
-Authorize authentification key received from a message
-Returns Boolean
-'''
-
-def authorize(info):
-  return info == "auth"
-
-'''
-Generate authentification key to send with messages
-Returns String
-'''
-
-def authenticate():
-  return "auth"
 
 #----------------------------------------------------------------------------------------------
 
