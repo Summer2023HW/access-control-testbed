a358751cd4a7fd310dac6d4c66a8107acdfe1f13
Mac <39278228+Reithger@users.noreply.github.com>
2019-07-08 15:03:10 -0300

    Timeout

--- Arbiter/ping_network.sh
+++ Arbiter/ping_network.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat ../network_ip.txt | while read output
 do
-	ping -q -c 1 $output >> trash.txt
+	ping -q -c 1 -i 0.2 $output >> trash.txt
 	if [ $? -eq 0 ]; then
 		echo "node $output is up"
 	else
