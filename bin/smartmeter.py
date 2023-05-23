b1a0476cd7a8a31a57615d919f4469271206941b
Hayden Walker <haydenwalker@live.com>
2023-05-23 10:22:39 -0300

    Made `Smartmeter` class

--- bin/smartmeter.py
+++ bin/smartmeter.py
@@ -1,86 +1,96 @@
 import _thread
 import socket
-import sys
 import re
-#sys.path.insert(0, "../Library")
 from access import *
 
-''' Value representing total consumption as recorded by this SmartMeter '''
-stored = 0
-''' Type of this entity in the network '''
-type = 'smart_meter'
-''' Specific id of this entity, specifying its nature '''
-id = ''
-''' Default port to connect to '''
-TCP_PORT = 5005
+class Smartmeter:
+  def __init__(self, name):
+    '''
+    Create a new smart meter
+    '''
 
-'''
-Main function that is called with set values for dynamic functioning; binds listening socket and responds to received messages.
-'''
+    # total consumption
+    self.stored = 0
+    
+    # type of entity
+    self.type = 'smart_meter'
+    
+    # meter name
+    self.id = name
+    
+    # port
+    self.TCP_PORT = 5005
 
-def start(set_id):
-  global id
-  id = set_id
-  sock = make_socket()
-  if(not bind_socket(sock, '', 12, TCP_PORT)):
-    print("Failure to bind local socket, program shutting down.")
-    return
+  def start(self):
+    '''
+    Start the smart meter
+    '''
+    sock = make_socket()
+    if(not bind_socket(sock, '', 12, self.TCP_PORT)):
+      print("Failure to bind local socket, program shutting down.")
+      return
 
-  while True:
-    conn, address = sock.accept()
-    conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    _thread.start_new_thread(process, (conn,))
+    while True:
+      conn, address = sock.accept()
+      conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      _thread.start_new_thread(self.process, (conn,))
 
-'''
-Method to handle incoming messages on an active socket
-'''
+  def process(self, sock):
+    '''
+    Handle incoming messages on an active socket
+    '''
 
-def process(sock):
-  while True:
-    info = receive(sock)
-    if(info == None):
-      continue
-    if(authorize(info[0])):
-      if(info[1] == "contact"):
-        send(sock, "")
-      elif(info[1] == "who"):
-        send(sock, type + split_term + id)
-      elif(info[1] == "symmetric"):
-        set_symmetric_key(sock.getpeername()[0], info[2])
-      elif(info[1] == "request"):
-        respond_status(sock)
-      elif(info[1] == "give"):
-        listen_appliance(sock, info)
+    while True:
+      info = receive(sock)
+      if(info == None):
+        continue
+      if(authorize(info[0])):
+        if(info[1] == "contact"):
+          send(sock, "")
+        elif(info[1] == "who"):
+          send(sock, type + split_term + id)
+        elif(info[1] == "symmetric"):
+          set_symmetric_key(sock.getpeername()[0], info[2])
+        elif(info[1] == "request"):
+          self.respond_status(sock)
+        elif(info[1] == "give"):
+          self.listen_appliance(sock, info)
 
+  def listen_appliance(self, sock, first):  
+    '''
+    Listen to an appliance (given an IP)
+    '''
+    self.process(first)
+    while True:
+      data = receive(sock)
+      if(data == None):
+        continue
+      self.record(data)
 
-'''
-Given an ip, sets up socket to be responsive and react to expected input from that source
-'''
+  def record(self, data):
+    '''
+    Process input
+    '''
+    global stored, id
+    if(authorize(data[0])):
+      val = str(re.search(id[0] + ":\d+", str(data)).group(0))
+      val = str(re.search("\d+", val).group(0))
+      stored += int(val)
+    print("Total " +  id  + " Count: " + str(stored))
 
-def listen_appliance (sock, first):
-  process(first)
-  while True:
-    data = receive(sock)
-    if(data == None):
-      continue
-    record(data)
+  def respond_status(self, sock):
+    '''
+    Respond with meter status
+    '''
+    send(sock, id  + split_term + "Usage:" + split_term + str(stored))
+    close_socket(sock)
 
-'''
-Given input that affects stored value of the smart_meter, processes it
-'''
+# Start if run directly
+if __name__ == '__main__':
+  # Attempt to start
+  try:
+    Smartmeter(sys.argv[1]).start()
 
-def record(data):
-  global stored, id
-  if(authorize(data[0])):
-    val = str(re.search(id[0] + ":\d+", str(data)).group(0))
-    val = str(re.search("\d+", val).group(0))
-    stored += int(val)
-  print("Total " +  id  + " Count: " + str(stored))
-
-'''
-Upon request, inform the caller of the status of this Smart Meter
-'''
-
-def respond_status (sock):
-  send(sock, id  + split_term + "Usage:" + split_term + str(stored))
-  close_socket(sock)
+  # Print error message if failed
+  except IndexError:
+    print(f'Usage: {sys.argv[0]} [name]', file=sys.stderr)
